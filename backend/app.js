bcdfe0d073e414fc392feb67c034c6375ad53d70
Francisco Gustavo <hidalgofco520@gmail.com>
2021-03-13 18:01:15 -0600

    fronten:feat: add page Home and Branches where we can se all branches

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const config = require('config');
 const app = express();
 
 const API_V1_ROUTES = require('./router');
 
+app.use(cors());
 app.use(morgan(config.get('logger')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
