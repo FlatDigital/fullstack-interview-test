767831178f2c1cbe5b96da2d0d3b7156f4f32e82
Eduard García <eddvidya@hotmail.com>
2021-05-18 13:48:44 -0400

    Added new urls and views

--- git_wrapper/wrapper/urls.py
+++ git_wrapper/wrapper/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 	path('', views.index, name = 'index'),
 	path('<str:branch>/', views.commits, name = 'commits'),
+	path('<str:branch>/commit/<str:commit_id>/', views.commit_details, name = 'commit_details'),
 ]
--- git_wrapper/wrapper/views.py
+++ git_wrapper/wrapper/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from git import Repo
 
 # Create your views here.
+def index(request):
+	repo = Repo('../.')
+	res = {
+		'desc': repo.description,
+		'branches': list(repo.branches),
+		'active_branch': repo.active_branch,
+		'remotes': list(repo.remotes),
+	}
+	return render(request, 'wrapper/index.html', res)
+
+def commits(request, branch):
+	repo = Repo('../.')
+	res = {
+		'branch': branch,
+		'commits': list(repo.iter_commits(branch))
+	}
+	return render(request, 'wrapper/commits.html', res)
+
+def commit_details(request, branch, commit_id):
+	repo = Repo('../.')
+	res = {
+		'commit': repo.commit(commit_id),
+		'branch': branch 
+	}
+	return render(request, 'wrapper/commit_details.html', res)
