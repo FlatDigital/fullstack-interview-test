563584804e3cc5da0f713b3b3a6d986f1a2f8961
Eduard García <eddvidya@hotmail.com>
2021-05-21 13:00:14 -0400

    Added prototype PR model

--- git_wrapper/wrapper/models.py
+++ git_wrapper/wrapper/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class PullRequest(models.Models):
+	OPEN = 'Open'
+	CLOSED = 'Closed'
+	status_list = [
+		(OPEN, 'Open'),
+		(CLOSED, 'Closed')
+	]
+
+	title = models.CharField(max_length=500)
+	owner = models.CharField(max_length=100)
+	status = models.CharField(max_length=10, choices=status_list)
+
+	class Meta:
+		managed = True
+		verbose_name = "Pull Request"
+		verbose_name_plural = "Pull Requests"
+		db_table = 'pull_request'
+	def __str__(self):
+		return self.title
