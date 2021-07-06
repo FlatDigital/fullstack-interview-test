e1ab9494040be4dd53f765ca5a3901569a6145fa
RaulS0sa <53208514+RaulS0sa@users.noreply.github.com>
2021-07-06 15:56:04 -0500

    AddedBranchView

--- GitView/views.py
+++ GitView/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
 from django.http import HttpResponse
 from git import Repo
 
@@ -8,5 +11,10 @@ def index(request):
     # For all you know, the first argument to Repo is a path to the repository
     # you want to work with
     repo = Repo('/Users/macbookair/Documents/GitHub/fullstack-interview-test')
+    branches = []
+    remote_branches = []
+    for ref in repo.git.branch('-r').split('\n'):
+        print(ref)
+        remote_branches.append(ref)
     assert not repo.bare
     return HttpResponse("Hello, world. You're at the polls index.")
