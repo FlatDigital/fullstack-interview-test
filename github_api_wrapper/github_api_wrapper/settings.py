5e21ee89431108847f6fe8d1ed7a75729d9f1290
HuairaGomez <huairagomez@gmail.com>
2021-10-30 22:22:19 -0300

    add django-environ

--- github_api_wrapper/github_api_wrapper/settings.py
+++ github_api_wrapper/github_api_wrapper/settings.py
@@ -10,21 +10,30 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import environ
 import os
 from pathlib import Path
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!e4bao)t6z1f)kk#)$nx=*+cfwbmp@iy_s6^g^zf_21%+^zhbf'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -129,3 +138,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GITHUB_TOKEN = env.str('GITHUB_TOKEN', default=False)
+GITHUB_OWNER= env.str('GITHUB_OWNER', default=False)
+GITHUB_REPOSITORY= env.str('GITHUB_REPOSITORY', default=False)
