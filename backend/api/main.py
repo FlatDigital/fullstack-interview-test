5a9e1b7af7e67e0de811d1a27af6a7e1677ff81f
Pili Sánchez <sanchezpili6@gmail.com>
2022-06-29 14:31:44 -0500

    Get branches endpoint

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,10 +1,29 @@
 from flask import Blueprint
-from flask import request
+from flask import jsonify
 import json
-
+from github import Github
+from dotenv import dotenv_values
 
 main_blueprint = Blueprint('main', __name__)
+g = Github(dotenv_values().get('GITHUB_TOKEN'))
+u = g.get_user()
+repo = u.get_repo("fullstack-interview-test")
+default_branch_date = repo.get_branch("master").commit.commit.author.date
+branches = list(repo.get_branches())
 
-@main_blueprint.route('/test', methods=['GET'])
-def test():
-    return 'Hola'
+
+@main_blueprint.route('/get_branches', methods=['GET'])
+def get_branches():
+    branches_names = []
+    for branch in branches:
+        date = branch.commit.commit.committer.date
+        status = 'up to date with default branch'
+        if date > default_branch_date:
+            status = 'ahead of default branch'
+        elif date < default_branch_date:
+            status = 'behind default branch'
+        elif branch.name == 'master':
+            status = 'this is the default branch'
+        branches_names.append({'name': branch.name, 'total_commits': branch.commit.stats.total,
+                               'date': branch.commit.commit.committer.date, 'status': status})
+    return jsonify(branches_names)
---
+++ frontend/src/views/BranchesView.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <v-card class="branches-card">
+      <v-card-title>
+        <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Search Branch"
+            single-line
+            hide-details
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table
+          disable-sort
+          :headers="headers"
+          :items="branches"
+          :search="search"
+          class="branches-table"
+      ></v-data-table>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BranchesView.vue",
+  data () {
+    return {
+      search: '',
+      headers: [
+        { text: 'Name', align: 'start', value: 'name'},
+        { text: 'Status', filterable: false,  value: 'upToDate' },
+      ],
+      branches: [
+        {
+          name: 'master',
+          upToDate: ''
+        },
+        {
+          name: 'dev',
+          upToDate: ''
+        },
+        {
+          name: 'feat/branches-view',
+          upToDate: ''
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+  .branches-card{
+    margin: 5% 10% 10% 10%;
+  }
+  .branches-table{
+    text-align: center;
+  }
+</style>
